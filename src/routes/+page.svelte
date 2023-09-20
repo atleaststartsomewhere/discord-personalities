2e78dddca498de9506e67e75d38e6030da73b2aa
Ryan <ryan@ryankoon.com>
2023-09-20 13:44:34 -0700

    canvas update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script>
-    import { Chart } from 'chart.js';
+    import { Chart } from 'chart.js/auto';
     import { onMount } from 'svelte';
 
     export let data;
-
-    let spiderChartElement: HTMLCanvasElement;
+    let chart;
 
     onMount(() => {
-        new Chart()
+        const ctx = document.getElementById('myChart').getContext('2d');
+
+        console.log(data);
+
+        chart = new Chart(ctx, data.chartConfig)
     });
+
 </script>
+<h1>Discord Personalities</h1>
+<canvas id="myChart" height="600" width="400"></canvas>
