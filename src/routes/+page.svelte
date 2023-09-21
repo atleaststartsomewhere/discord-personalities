1c555436f5399c987b6e9b6bec7994023c8b2801
Kevin Hernandez <kevin.hernandez288@gmail.com>
2023-09-20 19:39:21 -0700

    Added radial switcher for each chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,40 @@
     import LineChart from '$components/LineChart.svelte';
     import RadarChart from '$components/RadarChart.svelte';
     import LineChartStacked from '$components/LineChartStacked.svelte';
+
+    let radar = true;
+    let line = false;
+    let lineStacked = false;
+
 </script>
+
 <h1 class="text-4xl">Discord Personalities</h1>
 
-<h2>Radar</h2>
-<RadarChart></RadarChart>
+<div class="grid">
+    <div class="flex gap-6 items-center">
+        <div>
+            <label for="radar">Radar</label>
+            <input type="checkbox" name="radar" bind:checked={radar} id="radar">
+        </div>
+        <div>
+            <label for="line">Line</label>
+            <input type="checkbox" name="line" bind:checked={line} id="line">
+        </div>
+        <div>
+            <label for="line-stacked">Line Stacked</label>
+            <input type="checkbox" name="line-stacked" bind:checked={lineStacked} id="line-stacked">
+        </div>
+    </div>
+</div>
 
-<h2>Line</h2>
-<LineChart></LineChart>
-
-<h2>Line Stacked</h2>
-<LineChartStacked></LineChartStacked>
+<div class="flex">
+    {#if radar}
+        <RadarChart />
+    {/if}
+    {#if line}
+        <LineChart />
+    {/if}
+    {#if lineStacked}
+        <LineChartStacked />
+    {/if}
+</div>
